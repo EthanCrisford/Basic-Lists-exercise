ca154e4f07ed8153404da055104bc2887a0a7abf
EthanCrisford <476052@chrome.escg.ac.uk>
2023-01-20 16:00:01 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+List<string> list = new List<string>();
+
+list.Add("Pepperoni");
+list.Add("Margerita");
+list.Add("Hawaian");
+list.Add("Meat Feast");
+list.Add("Texas BBQ");
+
+foreach (string content in list)
+{
+    Console.WriteLine(content);
+}
